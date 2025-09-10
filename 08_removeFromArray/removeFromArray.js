80212091b095940caac991182571d03eb04a5cc9
schea85 <sumnachea85@gmail.com>
2025-09-09 23:54:56 -0400

    answered

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    let result = [];
+    
+    arr.forEach(item =>{
+        if (!num.includes(item)){
+            result.push(item);
+        }
+    });
+    return result;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
